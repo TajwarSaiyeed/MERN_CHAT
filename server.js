6c6d36465cb807a103490d5eb864c6b1330c3989
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-25 07:15:18 +0600

    dotenv add | create .env file

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const chats = require("./data/data");
 const app = express();
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
 
 app.get("/", (req, res) => {
   res.send("Server is running on http://localhost:5000");
@@ -9,6 +11,12 @@ app.get("/api/chat", (req, res) => {
   res.send(chats);
 });
 
-app.listen(5000, () => {
-  console.log(`http://localhost:5000`);
+app.get("/api/chat/:id", (req, res) => {
+  const id = req.params.id;
+  const singleChat = chats.find((c) => c._id === id);
+  res.send(singleChat);
+});
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
 });

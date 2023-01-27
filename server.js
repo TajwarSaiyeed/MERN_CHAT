3ba8ebe2983c4f03867d3d16c96d3b0c2b81551a
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-27 10:53:17 +0600

    DB add | server update

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const cors = require("cors");
 const chats = require("./data/data");
+const connectDB = require("./config/db");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5000;
+const colors = require("colors");
 
+connectDB();
 app.use(cors());
-app.use(express());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Server is running on http://localhost:5000");
@@ -22,5 +25,5 @@ app.get("/api/chat/:id", (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`http://localhost:${PORT}`);
+  console.log(`http://localhost:${PORT}`.yellow.bold);
 });

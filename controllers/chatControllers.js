acd3cbad378f8984b9f8eda98899d0b69b6958fd
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-02 05:45:08 +0600

    fetch Chats

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -67,8 +67,22 @@ const fetchChats = asyncHandler(async (req, res) => {
           $eq: req.user._id,
         },
       },
-    }).then((result) => res.send(result));
-  } catch (error) {}
+    })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password")
+      .populate("latestMessage")
+      .sort({ updatedAt: -1 })
+      .then(async (results) => {
+        results = await User.populate(results, {
+          path: "latestMessage.sender",
+          select: "name pic email",
+        });
+        res.status(200).send(results);
+      });
+  } catch (error) {
+    res.status(400);
+    throw new Error(error.message);
+  }
 });
 
 module.exports = {

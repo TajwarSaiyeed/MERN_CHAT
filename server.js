28ec16392d92ec5cccc72e608d13a50f87c02cba
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-04 22:29:15 +0600

    message controllers update | message routes | server update

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const PORT = process.env.PORT || 5000;
 const colors = require("colors");
 const userRoutes = require("./routes/userRoutes");
 const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 
 connectDB();
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/user", userRoutes);
 app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 app.use(notFound);
 app.use(errorHandler);

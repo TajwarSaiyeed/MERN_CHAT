52a788c0f06e95fdc98371bdbaf2354311dbfac9
TajwarSaiyeed <rjabid36@gmail.com>
2023-02-06 17:53:26 +0600

    update

--- server.js
+++ server.js
@@ -46,4 +46,26 @@ io.on("connection", (socket) => {
     socket.join(userData._id);
     socket.emit("connected");
   });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+  });
+
+  socket.on("typing", (room) => {
+    socket.in(room).emit("typing");
+  });
+
+  socket.on("stop typing", (room) => {
+    socket.in(room).emit("stop typing");
+  });
+
+  socket.on("new message", (newMessageRecieved) => {
+    let chat = newMessageRecieved.chat;
+    if (!chat.users) return console.log("Chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id == newMessageRecieved.sender._id) return;
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
 });
